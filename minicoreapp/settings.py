a88a8597b968932aadbc753c7fc9743d49d6eb84
matiassromo <matiasromo10@hotmail.com>
2024-12-24 15:39:43 -0500

    azure deploy

--- minicoreapp/settings.py
+++ minicoreapp/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6rdpnb+ph=6!*w#_nw_1%t_@2sgr4u4e9a6@*quz8pe^fm291t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['minicoreapp.azurewebsites.net']
 
 
 # Application definition
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
